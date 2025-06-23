6d2cbfbe16f041ae07c83a748e9684e0231a9064
Kalyani Sahu <kalyani.brm@gmail.com>
2025-06-23 19:20:23 +0530

    writing data in a file
    writing data

---
+++ NodeJs_Code/app.js
@@ -0,0 +1,56 @@
+const  http=require('http');
+const fs=require('fs')
+const server=http.createServer((req,res)=> {
+    
+     
+
+     if(req.url ==='/'){
+         res.setHeader('Content-Type','text/html');
+
+         res.end(
+            `
+            <form method="POST" action="/message">
+            <label>Name:</label>
+            <input type="text" name="username"></input>
+            <button type="submit">Add</button>
+            </form>
+            `
+         );
+
+     }else{
+        if(req.url =='/message'){
+
+            res.setHeader('Content-Type','text/html');
+
+            let  body=[];
+            req.on('data',(chunks)=>{
+                  body.push(chunks);
+            });
+
+            req.on('end',()=>{
+                let buffer=Buffer.concat(body);
+
+                console.log(buffer);
+
+                let formData=buffer.toString();
+                console.log(formData);
+
+                const formValues=formData.split('=')[1];
+
+                fs.writeFile('formValues.txt',formValues,(err)=>{
+                       
+                    res.statusCode=302 ;
+                    res.setHeader('Location','/');
+                    res.end();
+                })
+            })
+        }
+     }
+})
+
+server.listen(8000,() => {
+    console.log("server is running");
+});
+
+
+
