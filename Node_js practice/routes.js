4b58ed781f4dece61dbbaeb34dc408b9fa1f4158
Kalyani Sahu <kalyani.brm@gmail.com>
2025-06-29 20:00:14 +0530

    checking endpoints code
    checking endpoints code

--- Node_js practice/routes.js
+++ Node_js practice/routes.js
@@ -1,39 +1,31 @@
 
-const express=require('express')
-const app=express();
+const express = require('express');
+const app = express();
+
 
 app.use(express.json());
 
-let users=[
-    {id:1 , name:smith},
-    {id:2 , name:bos}
-];
-
-//GET all users
-app.get('/users',(req,res)=> {
-    res.json(users);
+// GET /orders
+app.get('/orders', (req, res) => {
+  res.send('Here is the list of all orders.');
 });
 
-app.get('/users/:id',(req,res)=> {
-    const userId=parseInt(req.params.id);
-    const user= users.find(u =>u.id === user.id);
-
-    if(!user){
-        return res.status(404).json({message:'user not found'});
-    }
-    res.json(user);
-})
-
-//POST to add a new user
-app.post('/users',(req,res)=>{
-    const {name}=req.body ;
-    const newuser={id:users.length+1 ,name};
-    users.push(newuser);
-    res.status(201).json(newuser);
+// POST /orders
+app.post('/orders', (req, res) => {
+  res.send('A new order has been created.');
 });
 
+// GET /users
+app.get('/users', (req, res) => {
+  res.send('Here is the list of all users.');
+});
 
+// POST /users
+app.post('/users', (req, res) => {
+  res.send('A new user has been added.');
+});
 
-app.listen(2000,()=>{
-    console.log("server is running");
-})
+// Start the server
+app.listen(6500, () => {
+  console.log('Server is running.');
+});
