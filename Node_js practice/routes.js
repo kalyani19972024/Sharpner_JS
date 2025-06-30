3786525e0a0dc24cf42da40f13052904ae4794cb
Kalyani Sahu <kalyani.brm@gmail.com>
2025-06-30 07:15:12 +0530

    Assignment on topic wild card
    Assignment on topic wild card

--- Node_js practice/routes.js
+++ Node_js practice/routes.js
@@ -1,31 +1,39 @@
 
-const express = require('express');
-const app = express();
+const express =require('express');
+const app =express();
 
 
-app.use(express.json());
+ app.use(express.json());
 
-// GET /orders
-app.get('/orders', (req, res) => {
-  res.send('Here is the list of all orders.');
+
+app.get('/products', (req, res) => {
+    res.send("Here is the list of all products.");
 });
 
-// POST /orders
-app.post('/orders', (req, res) => {
-  res.send('A new order has been created.');
+
+app.post('/products', (req, res) => {
+    res.send("A new product has been added.");
 });
 
-// GET /users
-app.get('/users', (req, res) => {
-  res.send('Here is the list of all users.');
+
+app.get('/categories', (req, res) => {
+    res.send("Here is the list of all categories.");
 });
 
-// POST /users
-app.post('/users', (req, res) => {
-  res.send('A new user has been added.');
+
+app.post('/categories', (req, res) => {
+    res.send("A new category has been created.");
 });
 
-// Start the server
-app.listen(6500, () => {
-  console.log('Server is running.');
+
+
+
+app.use((req, res) => {
+    res.status(404).send('<h1>404 - Page Not Found</h1>');
 });
+
+
+app.listen(3300, () => {
+    console.log('Server is running');
+});
+
