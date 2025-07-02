6f712253b1cd4db71bb298a05097eb5bbd9e7f34
Kalyani Sahu <kalyani.brm@gmail.com>
2025-07-02 20:32:21 +0530

    Get Request with HTML
    Get Request with HTML

---
+++ Node_js practice/server.js
@@ -0,0 +1,14 @@
+
+const  express=require('express');
+const path=require('path');
+const app=express();
+
+app.get('/api/products',(req,res)=> {
+    res.sendFile(path.join(__dirname,"view","product.html"));
+})
+
+
+
+app.listen(3100,() => {
+    console.log("server is listening");
+})
