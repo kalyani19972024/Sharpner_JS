7f60f18cdacf7b2c9af649a49673b4d79c959ab0
Kalyani Sahu <kalyani.brm@gmail.com>
2025-07-29 17:04:58 +0530

    Leaderboard optimization
    Leaderboard optimization

--- AWS assignment/controllers/premiumController.js
+++ AWS assignment/controllers/premiumController.js
@@ -1,18 +1,25 @@
 
 const User = require('../models/User');
-const {Expense}=require('../models/Expense');
+const Expense=require('../models/Expense');
 const Sequelize = require('sequelize');
+const sequelize = require('../utils/db');
 
 exports.getLeaderboard = async (req, res) => {
   try {
     const leaderboard = await User.findAll({
-      attributes: ['id', 'name', 'totalExpense'],
-       order: [['totalExpense', 'DESC']]
-      // include: [{ model: Expense, attributes: [] }],
-      // group: ['User.id'],
-      // order: [[Sequelize.literal('totalExpenses'), 'DESC']]
+      attributes: ['id', 'name', [sequelize.fn('SUM',sequelize.col('Expenses.amount')),'totalExpense']],
+      include:[
+             {
+                model:Expense ,
+                attributes:[]
+             }
+      ],
+       group:['User.id'],
+       order: [[sequelize.literal('totalExpense'), 'DESC']]
+       
     });
 
+    console.log(leaderboard);
     res.status(200).json(leaderboard);
   } catch (err) {
     console.error('Leaderboard error:', err);
