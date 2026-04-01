e2e89bc967bb9d380439d6b5aa7946b0223beff5
Kalyani Sahu <kalyani.brm@gmail.com>
2026-04-01 12:49:11 +0530

    Building the header of my restaurant app
    Building the header of my restaurant app

---
+++ Resturantweb/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+
+function Navbar(){
+ return (
+    <div>
+        <nav class="d-flex justify-content-between align-items-start p-3 px-4 bg-danger text-white">
+        <h1>Reactmeals</h1>
+           <div >
+            <button class=" text-light bg-dark btn btn-secondary btn-lg   position-relative">🛒 Your Cart
+                <button class="rounded-circle bg-danger text-white">
+                    0
+                </button>
+            </button>
+           </div>
+        </nav>
+         
+         {/* //for image */}
+        <div>
+            <img src="https://images.unsplash.com/photo-1504674900247-0877df9cc836"
+            alt="food" className="img-fluid w-100" style={{ height: "200px", objectFit: "cover" }}></img>
+        </div>
+    </div>
+    
+ )
+}
+
+export default Navbar ;
